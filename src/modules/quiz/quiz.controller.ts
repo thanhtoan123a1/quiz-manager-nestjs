3d714af6184198ef3ab11e68bd13dfe589ca28ef
thanhtoan1 <thanhtoan1@thanhtoan1s-MacBook-Air.local>
2023-02-07 14:22:57 +0700

    feat: create new service

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { CreateQuizDto } from './dto/CreateQuiz.dto';
+import { QuizService } from './quiz.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('quiz')
 export class QuizController {
+  constructor(private quizService: QuizService) {}
   @Get('/')
   getAllQuiz() {
-    return [1, 2, 3];
+    return this.quizService.getAllQuiz();
+  }
+  @Post('/create')
+  @UsePipes(ValidationPipe)
+  @HttpCode(200)
+  createQuiz(@Body() quizData: CreateQuizDto) {
+    return {
+      data: quizData,
+    };
   }
 }
--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,7 +1,9 @@
+import { QuizService } from './quiz.service';
 import { Module } from '@nestjs/common';
 import { QuizController } from './quiz.controller';
 
 @Module({
   controllers: [QuizController],
+  providers: [QuizService],
 })
 export class QuizModule {}
